37b7f9e9f8f288ea00540984c901f85975e73033
Siddharthv7 <demonreynard@gmail.com>
2026-05-21 17:06:23 +0530

    Updated frontend URL in CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -31,7 +31,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://your-frontend-url.onrender.com"
+        "https://ai-recruitment-platform-1-m70h.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
