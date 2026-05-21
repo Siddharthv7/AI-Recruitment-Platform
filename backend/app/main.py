0672608391b1e3feeadfde8b7c03196c71c6bc17
Siddharthv7 <demonreynard@gmail.com>
2026-05-21 17:10:50 +0530

    Fixed final CORS issue

--- backend/app/main.py
+++ backend/app/main.py
@@ -31,7 +31,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://ai-recruitment-platform-1-m70h.onrender.com"
+        "https://ai-recruitment-platform-1-m70h.onrender.com",
+        "https://ai-recruitment-platform-1-m70h.onrender.com/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
