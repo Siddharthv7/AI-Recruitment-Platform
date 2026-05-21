c917905806529e67043da7e62873c2794bbf18a7
Siddharthv7 <demonreynard@gmail.com>
2026-05-21 17:20:23 +0530

    Fixed CORS properly

--- backend/app/main.py
+++ backend/app/main.py
@@ -30,7 +30,7 @@ db = SessionLocal()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
